41fe320fa661d36031ad11f71fb7674f9bac9c72
DayBF <carlosjhr13@gmail.com>
2024-10-28 01:46:27 -0300

    assets into public

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,20 +10,20 @@ export const Home = () => {
   return (
     <div>
       <div className="flex w-full h-full sm:h-screen px-10 gap-40 sm:justify-between pt-20 md:pt-28 sm:pt-0">
-        {/* < className="flex w-full h-screen px-10 gap-40 bg-[url('/assets/icons/background.jpeg')]"> */}
+        {/* < className="flex w-full h-screen px-10 gap-40 bg-[url('./assets/icons/background.jpeg')]"> */}
         <div className="sm:flex flex-col sm:justify-end sm:items-center gap-4 mb-10 hidden">
           <span className="w-[1px] h-1/3 bg-gray-800"></span>
           <a href="https://www.linkedin.com/in/carlosjhr12/">
             <img
               className="w-4 h-4 min-w-4 xl:w-6 xl:h-6"
-              src={`/assets/icons/linkeding.svg`}
+              src={`./assets/icons/linkeding.svg`}
               alt="linkeding"
             />
           </a>
           <a href="https://wa.link/2ha34r">
             <img
               className="w-4 h-4 min-w-4 xl:w-6 xl:h-6"
-              src={`/assets/icons/whatsapp.svg`}
+              src={`./assets/icons/whatsapp.svg`}
               alt="whatsapp"
             />
           </a>
@@ -47,7 +47,7 @@ export const Home = () => {
           </div>
           <img
             className="h-[90%] self-end object-cover"
-            src={`/assets/imagenPerfil.png`}
+            src={`./assets/imagenPerfil.png`}
             alt="Carlos Hernandez"
           />
         </section>
